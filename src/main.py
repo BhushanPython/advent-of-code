faee8cd2f6eaf504b65c69e54d61579491830aca
BhushanPython <126884026+BhushanPython@users.noreply.github.com>
2023-05-29 17:30:21 +0530

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# Requires Python 3.10 or greater
+# run the day wise python files only from here
+
+# from days.day_1 import day_1
+from days.day_2 import day_2
+from days.day_3 import day_3
+
+
+def main():
+    # day_1()
+    # day_2()
+    day_3()
+    return
+
+
+if __name__ == '__main__':
+    main()
